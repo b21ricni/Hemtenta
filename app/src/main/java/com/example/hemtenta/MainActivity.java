45745601bcdf3aae85cce723568946b0f46ea9ad
b21ricni <b21ricni@student.his.se>
2022-06-01 13:40:47 +0200

    Added clickhandler

--- app/src/main/java/com/example/hemtenta/MainActivity.java
+++ app/src/main/java/com/example/hemtenta/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.hemtenta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button = findViewById(R.id.button);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "clicked about button");
+                Intent intent = new Intent(MainActivity.this, .class);
+                startActivity(intent);
+            }
+        });
     }
 }
